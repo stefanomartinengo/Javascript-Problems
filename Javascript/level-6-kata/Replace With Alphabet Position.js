28b46980b9dfb461ba38aa85cc2342d507d1c84d
Stefano <stefanomartinengo3@gmail.com>
2019-05-30 20:42:47 -0600

    lvl 6 2nd attempt

--- Javascript/level-6-kata/Replace With Alphabet Position.js
+++ Javascript/level-6-kata/Replace With Alphabet Position.js
@@ -3,6 +3,7 @@
 // with its position in the alphabet.
 // If anything in the text isn't a letter, ignore it and don't return it.
 
+// First Attempt
 function alphabetPosition(text) {
     let result = [];
     var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -14,4 +15,18 @@ function alphabetPosition(text) {
       };
     });
     return result.join(' ');
+  };
+
+// Revised cleaner attempt
+function alphabetPosition(text) {
+    let result = [];
+    let lowerCaseTxt = text.toLowerCase().split(' ').join('').split('');
+
+    lowerCaseTxt.map( letter => {
+      if(letter.match(/^[a-z]+$/)) {
+        result.push(letter.charCodeAt(0)-96);
+      };
+    });
+    return result.join(' ');
   }
+  alphabetPosition('The is just, something to test! okay?');
